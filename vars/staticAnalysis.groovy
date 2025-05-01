3d7c88ec57b35e0e36ee223c9c51e3659dadb463
unknown <natnov319@gmail.com>
2025-05-01 13:12:44 +0200

    B4: Añadida lógica para abortar según rama

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,8 +1,22 @@
-def call(Boolean abortPipeline = false) {
+def call(boolean abortPipeline = false) {
     withSonarQubeEnv('SonarQube') {
         echo 'Ejecución de las pruebas de calidad de código'
     }
+
+    // Esperar resultados de Quality Gate
     timeout(time: 5, unit: 'MINUTES') {
-        waitForQualityGate abortPipeline: abortPipeline
+        def qg = waitForQualityGate()
+        echo "Quality Gate status: ${qg.status}"
+
+        def branch = env.BRANCH_NAME ?: 'unknown'
+        echo "La rama actual es: ${branch}"
+
+        if (abortPipeline) {
+            error "Abortando pipeline porque abortPipeline = true"
+        } else if (branch == 'master' || branch.startsWith('hotfix')) {
+            error "Abortando pipeline porque la rama es master o empieza por hotfix"
+        } else {
+            echo "La rama no requiere abortar. Continuando..."
+        }
     }
 }
